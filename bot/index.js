16f756bf6f5da800e8324e53e0ec0ac3522d174c
Danette10 <dan.sebag1007@gmail.com>
2024-02-20 12:48:22 +0100

    add config to discord bot

---
+++ bot/index.js
@@ -0,0 +1,19 @@
+import {Client, GatewayIntentBits} from 'discord.js';
+import config from '../config.json' assert {type: 'json'};
+
+const { TOKEN } = config;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(TOKEN);
