db90d2f5c3ab682a501f24dfedba5800a5978628
DorianLC <dorianlrc@yahoo.fr>
2024-05-22 21:05:19 +0200

    liveStream bot #1

--- commands.js
+++ commands.js
@@ -51,6 +51,18 @@ export const commands = [
         ],
     },
     {
+        name: 'livestream',
+        description: 'Diffuse en direct la webcam d\'un client.',
+        options: [
+            {
+                name: 'client_id',
+                description: 'Le numéro du client.',
+                type: 3,
+                required: true,
+            },
+        ],
+    },
+    {
         name: 'clients',
         description: 'Liste des clients.',
         options: [
